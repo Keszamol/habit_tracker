15eea1d21b2142cdfaf1261d19b58712011591cf
Celine Maloszek <celinemaloszek@outlook.com>
2025-05-06 00:45:22 +0200

    User Datenbanktabelle erstellt

---
+++ db/create_db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS users (
+               id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+               username TEXT NOT NULL UNIQUE
+               password TEXT NOT NULL
+               date TEXT NOT NULL DEFAULT (CURRENT_DATE)
+               )
+               ''')
+
+conn.commit()
+conn.close()
